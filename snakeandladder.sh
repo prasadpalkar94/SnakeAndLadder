88cf2f0e6ed7c690ba8cc31d71bcfaefd1e45263
prasadpalkar94 <explorehorizon15@gmail.com>
2020-01-31 18:00:23 +0530

    ADDED:Options Checked whether NO Play Or Snake Or Ladder

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -1,4 +1,23 @@
 #!/bin/bash -x
 echo "------------------------------------------Welcome To SnakeAndLadder------------------------------------------"
 player1_start_pos=0
+NO_PLAY=0
+SNAKE=1
+LADDER=2
+pos=$player1_start_pos
 rolldie=$((RANDOM%6+1))
+
+function checkOptions(){
+options=$((RANDOM%3))
+if [ $options -eq 0 ]
+then
+	pos=$player1_start_pos
+elif [ $options -eq 1 ]
+then 
+	pos=$(($pos+$rolldie))
+else
+	pos=$(($pos-$rolldie))
+fi
+}
+ 
+checkOptions
