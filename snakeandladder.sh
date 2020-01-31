d9306d283447dd1c00d07f070df5a4ea72eca8e9
prasadpalkar94 <explorehorizon15@gmail.com>
2020-01-31 18:43:10 +0530

    REFACTORED:Variable to Constant Variable and Function Condition

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -1,23 +1,30 @@
 #!/bin/bash -x
 echo "------------------------------------------Welcome To SnakeAndLadder------------------------------------------"
-player1_start_pos=0
+PLAYER1_START_POS=0
+WIN_POS=100
 NO_PLAY=0
 SNAKE=1
 LADDER=2
-pos=$player1_start_pos
+pos=$PLAYER1_START_POS
 rolldie=$((RANDOM%6+1))
 
 function checkOptions(){
 options=$((RANDOM%3))
-if [ $options -eq 0 ]
-then
-	pos=$player1_start_pos
-elif [ $options -eq 1 ]
-then 
-	pos=$(($pos+$rolldie))
-else
-	pos=$(($pos-$rolldie))
-fi
+while [ $pos -le $WIN_POS ]
+do
+	if [ $options -eq 0 ]
+	then
+		pos=$PLAYER1_START_POS
+	elif [ $options -eq 2 ]
+	then 
+		pos=$(($pos+$rolldie))
+	else
+		pos=$(($pos-$rolldie))
+		if [ $pos -lt 0 ]
+		then 
+			pos=$PLAYER1_START_POS
+		fi
+	fi
+done
 }
- 
 checkOptions
