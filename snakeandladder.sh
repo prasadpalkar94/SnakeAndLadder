b328eb41492ec8e2221db4b4c0e3ba7928a65ad0
prasadpalkar94 <explorehorizon15@gmail.com>
2020-02-01 16:24:07 +0530

    REFACTORED:functions and ADDED:dictionary

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -1,35 +1,42 @@
-#!/bin/bash -x
-echo "------------------------------------------Welcome To SnakeAndLadder------------------------------------------"
+#!/bin/bash -x  
+echo "------------------Welcome To SnakeAndLadder-----------------------"
+declare -A dice
 PLAYER1_START_POS=0
 WIN_POS=100
-NO_PLAY=0
-SNAKE=1
+NO_PLAY=1
 LADDER=2
+SNAKE=3
 pos=$PLAYER1_START_POS
-rolldie=$((RANDOM%6+1))
+
+function roll(){
+randomCheck=$((RANDOM%6+1))
+echo $randomCheck
+}
+
 
 function checkOptions(){
-options=$((RANDOM%3))
-while [[ $pos -lt $WIN_POS ]]
+while [[ $pos -ne $WIN_POS ]]
 do
-	if [ $options -eq 0 ]
-	then
-		pos=$PLAYER1_START_POS
-	elif [ $options -eq 2 ]
-	then 
-		pos=$(($pos+$rolldie))
-		if [ $pos -gt $WIN_POS ]
-		then
-		pos=$(($pos-$rolldie))
-      fi
-	else
-		pos=$(($pos-$rolldie))
-		if [ $pos -lt $PLAYER1_START_POS ]
-		then 
-			pos=$PLAYER1_START_POS
-		fi
-	fi
-		echo "player position:" $pos
+options=$((RANDOM%3+1))
+			((total++))
+	case $options in 
+			  $NO_PLAY)pos=$PLAYER1_START_POS ;;
+			  $LADDER)pos=$((pos+$(roll)))   ;;
+			  $SNAKE)pos=$((pos-$(roll)))   ;;
+	esac
+   if [[ $pos -lt $PLAYER1_START_POS ]]
+   then 
+      pos=$PLAYER1_START_POS
+   fi
+
+	 if [[ $pos -gt $WIN_POS ]]
+   then 
+      pos==$((pos-$(roll)))
+   fi
+
+	dice[$total]=$pos
 done
 }
 checkOptions
+${!dice[@]}
+${dice[@]}
